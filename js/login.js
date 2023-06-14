e51728578ddd578fc28c3ed80f3da5ddb555f88e
김민준 <alswns03@sungkyul.ac.kr>
2023-06-14 08:32:29 +0000

    수업이외의추가구현완료

--- js/login.js
+++ js/login.js
@@ -1,61 +1,126 @@
-	function login(){
-    let form = document.querySelector("#form_main");
-    let id = document.querySelector("#floatingInput");
-    let password = document.querySelector("#floatingPassword");
-	let check = document.querySelector("#idSaveCheck");
-		
-	form.action = "../index_login.html";
-    form.method = "get";
-		
-	if(check.checked == true) {
-		alert("쿠키를 저장합니다.");
-		setCookie("id", id.value, 1);
-		alert("쿠키 값 :" + id.value);
-	}
-	else {
-		setCookie("id", id.value, 0);
-	}
-	
-	if(id.value.length === 0 || password.value.length === 0){
-		alert("아이디와 비밀번호를 모두 입력해주세요.")
-	}else{
-		session_set();
-		form.submit();
-	}
-	}
-	function logout(){
-	session_del();
-	location.href='../index.html';
+function login_check() {
+  var email = document.querySelector("#floatingInput").value;
+  var password = document.querySelector("#floatingPassword").value;
+
+  var emailPattern = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  var passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$/;
+
+  if (!emailPattern.test(email)) {
+    alert("유효한 이메일 형식이 아닙니다.");
+    return false; // 유효성 검사 실패 시 false 반환
+  }
+
+  if (!passwordPattern.test(password)) {
+    alert("유효한 패스워드 형식이 아닙니다.");
+    return false; // 유효성 검사 실패 시 false 반환
+  }
+
+  return true; // 유효성 검사 통과 시 true 반환
 }
-function get_id(){
-	if(true){
-		decrypt_text();
-	}
-	else{
-    var getParameters = function(paramName){ // 변수 = 함수(이름)
-    var returnValue; // 리턴값을 위한 변수 선언
-    var url = location.href; // 현재 접속 중인 주소 정보 저장
-    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
-        for(var i = 0; i < parameters.length; i++) { 
-		    var varName = parameters[i].split('=')[0];
-            
-            if (varName.toUpperCase() == paramName.toUpperCase()) {
-                returnValue = parameters[i].split('=')[1];
-                return decodeURIComponent(returnValue);
-            // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
-		    }
-	    } // 2중 for문 끝
-}; // 함수 끝
-alert(getParameters('id') + '님 반갑습니다!');
-	}
-}	
-function addJavascript(jsname) { // 자바스크립트 외부 연동
-	var th = document.getElementsByTagName('head')[0];
-	var s = document.createElement('script');
-	s.setAttribute('type','text/javascript');
-	s.setAttribute('src',jsname);
-	th.appendChild(s);
+
+function login() {
+  // login_check 함수 호출하여 유효성 검사 수행
+  if (!login_check()) {
+    return; // 유효성 검사 실패 시 함수 종료
+  }
+
+  // 유효성 검사 통과 후 로그인 절차 진행
+  let form = document.querySelector("#form_main");
+  let id = document.querySelector("#floatingInput");
+  let password = document.querySelector("#floatingPassword");
+  let check = document.querySelector("#idSaveCheck");
+
+  form.action = "../index_login.html";
+  form.method = "get";
+
+  if (check.checked == true) {
+    alert("쿠키를 저장합니다.");
+    setCookie("id", id.value, 1);
+    alert("쿠키 값: " + id.value);
+  } else {
+    setCookie("id", id.value, 0);
+  }
+
+  if (id.value.length === 0 || password.value.length === 0) {
+    alert("아이디와 비밀번호를 모두 입력해주세요.");
+  } else {
+    session_set();
+    form.submit();
+  }
 }
-addJavascript('/js/security.js'); // 암복호화 함수
-addJavascript('/js/session.js'); // 세션 함수
-addJavascript('/js/cookie.js'); // 쿠키 함수
+function logout() {
+  session_del();
+  location.href = '../index.html';
+}
+
+function get_id() {
+  if (true) {
+    decrypt_text();
+  } else {
+    var getParameters = function (paramName) {
+      var returnValue;
+      var url = location.href;
+      var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
+      for (var i = 0; i < parameters.length; i++) {
+        var varName = parameters[i].split('=')[0];
+
+        if (varName.toUpperCase() == paramName.toUpperCase()) {
+          returnValue = parameters[i].split('=')[1];
+          return decodeURIComponent(returnValue);
+        }
+      }
+    };
+    alert(getParameters('id') + '님 반갑습니다!');
+  }
+}
+// 로그인 횟수 저장 함수
+function login_count() {
+  // 기존 쿠키의 카운트 값을 얻는다.
+  var count = getCookie("login_cnt");
+
+  if (count) {
+    // 쿠키 값이 존재하면 값을 증가시킨다.
+    count = parseInt(count) + 1;
+  } else {
+    // 쿠키 값이 존재하지 않으면 초기값을 1로 설정한다.
+    count = 1;
+  }
+
+  // 쿠키의 값을 +1 업데이트한다.
+  setCookie("login_cnt", count, 1);
+
+  // 업데이트된 카운트 값을 출력하거나 사용할 수 있다.
+  console.log("로그인 횟수: " + count);
+}
+
+// 로그아웃 횟수 저장 함수
+function logout_count() {
+  // 기존 쿠키의 카운트 값을 얻는다.
+  var count = getCookie("logout_cnt");
+
+  if (count) {
+    // 쿠키 값이 존재하면 값을 증가시킨다.
+    count = parseInt(count) + 1;
+  } else {
+    // 쿠키 값이 존재하지 않으면 초기값을 1로 설정한다.
+    count = 1;
+  }
+
+  // 쿠키의 값을 +1 업데이트한다.
+  setCookie("logout_cnt", count, 1);
+
+  // 업데이트된 카운트 값을 출력하거나 사용할 수 있다.
+  console.log("로그아웃 횟수: " + count);
+}
+
+function addJavascript(jsname) {
+  var th = document.getElementsByTagName('head')[0];
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', jsname);
+  th.appendChild(s);
+}
+
+addJavascript('/js/security.js');
+addJavascript('/js/session.js');
+addJavascript('/js/cookie.js');
--- js/session.js
+++ js/session.js
@@ -1,63 +1,134 @@
-function session_set(){
-	let id = document.querySelector("#floatingInput");
-    let password = document.querySelector("#floatingPassword");
-	let random = new Date();
-	
-	const obj = {
-		id : id.value,
-		otp : random
-	}
-	
-    if (sessionStorage) {
-		const objString = JSON.stringify(obj);
-        let en_text = encrypt_text(objString);
-		sessionStorage.setItem("Session_Storage_test", objString);
-        sessionStorage.setItem("Session_Storage_test", en_text);
-	} else {
-		alert("로컬 스토리지 지원 x");
-	}
+function session_set() {
+  let id = document.querySelector("#floatingInput");
+  let password = document.querySelector("#floatingPassword");
+  let random = new Date();
+
+  const obj = {
+    id: id.value,
+    otp: random,
+  };
+
+  if (sessionStorage) {
+    const objString = JSON.stringify(obj);
+    let en_text = encrypt_text(objString);
+    sessionStorage.setItem("Session_Storage_test", objString);
+    sessionStorage.setItem("Session_Storage_encrypted", en_text);
+  } else {
+    alert("세션 스토리지 지원 x");
+  }
 }
-function session_get(){
-	if (sessionStorage) {
-		return sessionStorage.getItem("Session_Storage_encrypted");
-	} else {
-		alert("세션 스토리지 지원 x");
-	}
+
+function session_get() {
+  if (sessionStorage) {
+    return sessionStorage.getItem("Session_Storage_encrypted");
+  } else {
+    alert("세션 스토리지 지원 x");
+  }
 }
-function session_check(){
-	if (sessionStorage.getItem("Session_Storage_test")){
-		alert("이미 로그인 되었습니다.");
-		location.href='index_login.html';
-	}
+
+function session_check() {
+  if (sessionStorage.getItem("Session_Storage_test")) {
+    alert("이미 로그인 되었습니다.");
+    location.href = 'index_login.html';
+  }
 }
+
 function session_del() {
-	if (sessionStorage) {
-		sessionStorage.removeItem("Session_Storage_test");
-		alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
-	} else {
-		alert( " 세션 스토리지 지원 x");
-	}
+  if (sessionStorage) {
+    sessionStorage.removeItem("Session_Storage_test");
+    alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+  } else {
+    alert("세션 스토리지 지원 x");
+  }
 }
-function session_join_set(){ //세션 저장(객체)    
-    let f_name = document.querySelector("#firstName").value;
-    let l_name = document.querySelector("#lastName").value;
-    let b_day = document.querySelector("#birthdayDate").value;
-    let gender = document.querySelector("#inlineRadioOptions");
-    let email = document.querySelector("#emailAddress").value;
-    let p_number = document.querySelector("#phoneNumber").value;
-    let class_check = document.querySelector(".select form-control-lg");
-    let random = new Date(); // 랜덤 타임스탬프
-    
-    const newSignUp = new SignUp(f_name, l_name, b_day, gender, email, p_number, class_check, random);
-    console.log(newSignUp.fullName); // John Doe
-    console.log(newSignUp.contactInfo); // johndoe@email.com 123-456-7890
-    
-    if (sessionStorage) {
-        const objString = JSON.stringify(newSignUp); // 객체 -> JSON 문자열 변환
-        let en_text = encrypt_text(objString); // 암호화
-        sessionStorage.setItem("Session_Storage_object", objString);
-        sessionStorage.setItem("Session_Storage_encryted", en_text);
+
+function session_join_set() {
+  let f_name = document.querySelector("#firstName").value;
+  let l_name = document.querySelector("#lastName").value;
+  let b_day = document.querySelector("#birthdayDate").value;
+  let gender = document.querySelector("#inlineRadioOptions");
+  let email = document.querySelector("#emailAddress").value;
+  let p_number = document.querySelector("#phoneNumber").value;
+  let class_check = document.querySelector(".select form-control-lg");
+  let random = new Date();
+
+  const newSignUp = new SignUp(f_name, l_name, b_day, gender, email, p_number, class_check, random);
+  console.log(newSignUp.fullName); // John Doe
+  console.log(newSignUp.contactInfo); // johndoe@email.com 123-456-7890
+
+  if (sessionStorage) {
+    const objString = JSON.stringify(newSignUp); // 객체 -> JSON 문자열 변환
+    let en_text = encrypt_text(objString); // 암호화
+    sessionStorage.setItem("Session_Storage_object", objString);
+    sessionStorage.setItem("Session_Storage_encrypted", en_text);
+  } else {
+    alert("세션 스토리지 지원 x");
+  }
+}
+
+function createSession() {
+  let id = document.querySelector("#floatingInput");
+  let password = document.querySelector("#floatingPassword");
+  let random = new Date();
+
+  const obj = {
+    id: id.value,
+    otp: random,
+  };
+
+  if (sessionStorage) {
+    const objString = JSON.stringify(obj);
+    let en_text = encrypt_text(objString);
+    sessionStorage.setItem("Session_Storage_test", en_text);
+  } else {
+    alert("세션 스토리지 지원 x");
+  }
+}
+
+function getSession() {
+  if (sessionStorage) {
+    let en_text = sessionStorage.getItem("Session_Storage_test");
+    let objString = decrypt_text(en_text);
+    return JSON.parse(objString);
+  } else {
+    alert("세션 스토리지 지원 x");
+    return null;
+  }
+}
+
+function checkLoginTime() {
+  let sessionData = getSession();
+  if (sessionData) {
+    let loginTime = new Date(sessionData.otp);
+    let currentTime = new Date();
+    let sessionTimeout = 5 * 60 * 1000; // 5분을 밀리초로 변환
+    if (currentTime - loginTime > sessionTimeout) {
+      // 로그인 시간이 지났을 경우
+      logout();
     } else {
-        alert("세션 스토리지 지원 x");
-    }   
+      // 로그인 시간 유지 중
+      // 추가적인 작업을 수행하거나 필요한 로직을 구현할 수 있습니다.
+    }
+  } else {
+    // 세션 데이터가 없는 경우 (비로그인 상태)
+    // 로그인 페이지로 이동하거나 필요한 로직을 구현할 수 있습니다.
+  }
+}
+
+// 페이지 로드 시 로그인 시간 체크 수행
+window.onload = checkLoginTime;
+
+function session_join_get() {
+  if (sessionStorage) {
+    const objString = sessionStorage.getItem("Session_Storage_object");
+    if (objString) {
+      const de_text = decrypt_text(objString); // 복호화
+      const obj = JSON.parse(de_text); // JSON 문자열 -> 객체 변환
+      console.log(obj);
+    } else {
+      console.log("세션 스토리지에 가입 정보가 없습니다.");
+    }
+  } else {
+    console.log("세션 스토리지를 지원하지 않습니다.");
+  }
 }
